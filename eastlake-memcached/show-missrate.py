aac269b7e944e59757027337a9743128e0bd057d
zxm <527610259@qq.com>
2020-08-22 13:26:52 +0800

    add show missrate

--- eastlake-memcached/show-missrate.py
+++ eastlake-memcached/show-missrate.py
@@ -10,22 +10,25 @@ plt.ion()
 plt.figure(1)
 x_list = []
 y_list = []
-file_x = open("curr_items_time", "w")
-file_y = open("curr_items", "w")
+file_x = open("missrate_time", "w")
+file_y = open("missrate", "w")
 x = 0
 while True:
     x_list.append(x)
-    y = float(mc.get_stats()[0][1]['curr_items'])
-    y = y/1000000
+    cmd_get = float(mc.get_stats()[0][1]['cmd_get'])
+    get_misses = float(mc.get_stats()[0][1]['get_misses'])
+    if cmd_get != 0:
+	y = get_misses/cmd_get
+    else:
+	y = 0
+    y = y * 100
     y_list.append(y)
     file_x.write(str(x)+"\n")
     file_y.write(str(y)+"\n")
     plt.plot(x_list, y_list,'b--',mfc='w')
     plt.xlabel("Time(s)")
-    plt.ylabel("number(M)")
-    plt.title("memcached curr_items")
+    plt.ylabel("miss rate")
+    plt.title("memcached get miss rate")
     plt.show()
     plt.pause(1)
     x += 1
-
-    
