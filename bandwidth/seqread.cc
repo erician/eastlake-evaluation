692ebc472c983cdf581f43ff5c4f2c6015b99c23
liuchaojie <zkliuchaojie@163.com>
2021-04-19 19:45:32 +0800

    modified bw tests

--- bandwidth/seqread.cc
+++ bandwidth/seqread.cc
@@ -6,21 +6,25 @@
 #include <sys/time.h>
 #include <thread>
 #include <string.h>
+#include <sched.h>
+#include <mutex>
 
-#define PO_NAME             "seqread"
-// #define PO_SIZE             (16*1024*1024*1024L)
-// #define EXTEND_MAP_SIZE     (1*1024*1024*1024L)
-#define PO_SIZE             (160*1024*1024L)
-#define EXTEND_MAP_SIZE     (10*1024*1024L)
+#define PO_NAME             "bw_seqread"
+#define PO_SIZE             (16*1024*1024*1024L)
+#define EXTEND_MAP_SIZE     (1*1024*1024*1024L)
+// #define PO_SIZE             (1600*1024*1024L)
+// #define EXTEND_MAP_SIZE     (100*1024*1024L)
 #define BLOCK_SIZE          (4096L)
 #define BLOCK_NUM           (EXTEND_MAP_SIZE/BLOCK_SIZE)
 #define NR_THREAD           (16L)
+#define MAP_NUMA_AWARE         0x400000
 
-const int thread_num_cnt = 5;
-const int thread_num[5] = {1, 2, 4, 8, 16};
+const int thread_num_cnt = 16;
+const int thread_num[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
 
 // record chunk addr
 char *addrs[PO_SIZE/EXTEND_MAP_SIZE];
+std::mutex g_mutex;
 
 double my_second() {
     struct timeval tp;
@@ -29,25 +33,36 @@ double my_second() {
     return ( (double)tp.tv_sec + (double)tp.tv_usec*1.e-6);
 }
 
-void Prepare() {
-    printf("start to Prepare\n");
-    int pod = po_creat(PO_NAME, 0);
-    if (pod == -1) {
-        printf("Prepare po_creat, errno: %d\n", errno);
+/* in our system
+ * node0: 0-17, 36-53
+ * node1: 18-35, 54-71
+ * 16 threads : 0-15
+ */
+void set_affinity(uint32_t idx) {
+    cpu_set_t my_set;
+    CPU_ZERO(&my_set);
+    if (idx % 2 != 0) {
+        CPU_SET(idx + 18, &my_set);
+    } else {
+        CPU_SET(idx, &my_set);
+    }
+    sched_setaffinity(0, sizeof(cpu_set_t), &my_set);
+}
+
+void Prepare(int pod, int thread_id, int all_threads_num) {
+    set_affinity(thread_id);
+    g_mutex.lock();
+
+    char *addr = (char *)po_extend(pod, EXTEND_MAP_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_NUMA_AWARE|MAP_HUGETLB);
+    if (addr == NULL || addr < 0) {
+        printf("Prepare po_extend, errno: %d\n", errno);
         exit(-1);
+    } else {
+        for(long j = 0; j < EXTEND_MAP_SIZE; j++)
+            addr[j] = j;
+        addrs[thread_id] = addr;
     }
-    for (int i=0; i<PO_SIZE/EXTEND_MAP_SIZE; i++) {
-        char *addr = (char *)po_extend(pod, EXTEND_MAP_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE);
-        if (addr == NULL || addr < 0) {
-            printf("Prepare po_extend, errno: %d\n", errno);
-            exit(-1);
-        } else {
-            for(long j = 0; j < EXTEND_MAP_SIZE; j++)
-                addr[j] = j;
-            addrs[i] = addr;
-        }
-    }
-    po_close(pod);
+    g_mutex.unlock();
 }
 
 void Cleanup() {
@@ -59,30 +74,46 @@ void Cleanup() {
 }
 
 void SeqReadThread(int pod, int thread_id, int all_threads_num) {
-    long each_thread_rw_extend_num = PO_SIZE/EXTEND_MAP_SIZE/all_threads_num;
+    set_affinity(thread_id);
+    // each one deal with one EXTEND_MAP_SIZE
     char *buff = (char *)malloc(sizeof(char) * BLOCK_SIZE);
-    for(long i=0; i<each_thread_rw_extend_num; i++) {
-        for (long j=0; j<BLOCK_NUM; j++) {
-            memcpy(buff, addrs[each_thread_rw_extend_num*thread_id+i]+j*BLOCK_SIZE, BLOCK_SIZE);
-        }
+    for (long i = 0; i < BLOCK_NUM; i++) {
+        memcpy(buff, addrs[thread_id] + i * BLOCK_SIZE, BLOCK_SIZE);
     }
 }
 
+
+
 int main() {
-    Prepare();
+    // creat the po
     double start, end;
     int pod;
     std::thread *threads[NR_THREAD];
+
+    printf("start to Prepare\n");
+    pod = po_creat(PO_NAME, 0);
+    if (pod == -1) {
+        printf("Prepare po_creat, errno: %d\n", errno);
+        exit(-1);
+    }
+    for(long long int i=0; i<thread_num[thread_num_cnt-1]; i++)
+        threads[i] = new std::thread(Prepare, pod, i, thread_num[thread_num_cnt-1]);
+    for(int i=0; i<thread_num[thread_num_cnt-1]; i++)
+        threads[i]->join();
+    po_close(pod);
+    printf("Prepare Over\n");
+    
     for(int t=0; t<thread_num_cnt; t+=1) {
         start = my_second();
         pod = po_open(PO_NAME, O_CREAT|O_RDWR, 0);
+        
         for(long long int i=0; i<thread_num[t]; i++)
             threads[i] = new std::thread(SeqReadThread, pod, i, thread_num[t]);
         for(int i=0; i<thread_num[t]; i++)
             threads[i]->join();
         end = my_second();
         printf("thread number: %d, po size: %ld(MB), time: %lf(s), bandwidth: %lf(MB)\n", \
-            thread_num[t], PO_SIZE/1024/1024, end-start, PO_SIZE/(end-start)/1024/1024);
+            thread_num[t], (EXTEND_MAP_SIZE*thread_num[t])/1024/1024, end-start, (EXTEND_MAP_SIZE*thread_num[t])/(end-start)/1024/1024);
         po_close(pod);
     }
 
